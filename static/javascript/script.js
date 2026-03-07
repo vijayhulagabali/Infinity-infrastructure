af295de697fb83279c938f9ebef694a86b31781a
Vijay Hulagabali <vijayhulagabali2004@gmail.com>
2026-03-08 00:57:44 +0530

    Update script.js

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -61,7 +61,7 @@ function ValidateForm() {
 
 async function sendContactViaBackend(payload) {
     try {
-        const res = await fetch("https://infinity-infrastructure-3.onrender.com/api/contact")", {
+        const res = await fetch("https://infinity-infrastructure-3.onrender.com/api/contact", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(payload)
